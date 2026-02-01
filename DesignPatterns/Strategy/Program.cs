fd01d065976c1c985a951cf0b5b25aa2b73cbf16
Mohamed Saad <ms.youssef.c@nhci.sa>
2026-02-01 09:28:21 +0300

    feature: add startegy pattern

---
+++ DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,7 @@
+﻿using Strategy;
+
+var order = new Order("Marvin Software", 5, "Visual Studio License");
+order.Export(new CSVExportService());
+order.Export(new JsonExportService());
+
+Console.ReadLine();
